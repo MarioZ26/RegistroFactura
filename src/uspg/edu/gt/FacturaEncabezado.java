7633c4bc75b4a2349ee6c15d6a487233c2f6e2c0
Josue <oeramirezz@alumno.uspg.edu.gt>
2020-10-17 14:32:00 -0600

    parte 1 Otto

--- src/uspg/edu/gt/FacturaEncabezado.java
+++ src/uspg/edu/gt/FacturaEncabezado.java
@@ -11,11 +11,74 @@ package uspg.edu.gt;
  */
 public class FacturaEncabezado {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
+    int serie;
+    int numero;
+    String fecha;
+    String nombre;
+    String direccion;
+    int nit;
+
+    public FacturaEncabezado(int serie, int numero, String fecha, String nombre, String direccion, int nit) {
+        this.serie = serie;
+        this.numero = numero;
+        this.fecha = fecha;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.nit = nit;
+    }
+
+    
+
+    public int getSerie() {
+        return serie;
+    }
+
+    public void setSerie(int serie) {
+        this.serie = serie;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getNit() {
+        return nit;
+    }
+
+    public void setNit(int nit) {
+        this.nit = nit;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
     
+    
+    
+    
 }
+
---
+++ src/uspg/edu/gt/Principal.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uspg.edu.gt;
+
+/**
+ *
+ * @author Otto
+ */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        List<FacturaEncabezado> datosFac = new ArrayList();
+
+        int serie;
+        int numero;
+        String fecha;
+        String nombre;
+        String direccion;
+        int nit;
+        char ingresar = 'n';
+        int opcion = 0;
+
+        do {
+            System.out.println("Bienvenido al registro de facturas");
+            System.out.println("Seleccione la opcion que necesita");
+            System.out.println("----------------------------------");
+            System.out.println("1.Grabar datos");
+            System.out.println("2.Mostrar datos");
+            System.out.println("----------------------------------");
+            opcion = input.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("------------------------------------");
+                    System.out.println("Ingrese los datos del nuevo registro");
+                    System.out.println("------------------------------------");
+                    do {
+                        System.out.println("Serie:");
+                        serie = input.nextInt();
+                        System.out.println("Numero de la factura:");
+                        numero = input.nextInt();
+                        System.out.println("Fecha:");
+                        fecha = input.next();
+                        System.out.println("Nombre:");
+                        nombre = input.next();
+                        System.out.println("Direccion:");
+                        direccion=input.nextLine();
+                        System.out.println("Ingresar Nit:");
+                        nit = input.nextInt();
+
+                        datosFac.add(new FacturaEncabezado(serie, numero, fecha, nombre, direccion, nit));
+
+                        System.out.println("Desea agregar más datos si/no:");
+                        ingresar = input.next().charAt(0);
+                    } while (ingresar == 's');
+                    break;
+                case 2:
+                    System.out.println("--------------------------------");
+                    System.out.println("Datos de las facturas ingresadas");
+                    System.out.println("--------------------------------");
+                    
+                    
+                    System.out.println("Serie\t"+"No factura\t"+"Fecha\t"+"Nombre\t"+"Direccion\t"+"Nit\t");
+                    
+                    for (FacturaEncabezado facturaEncabezado: datosFac) {
+                        
+                        System.out.println(facturaEncabezado.getSerie()+"\t"+facturaEncabezado.getNumero()+"\t"+facturaEncabezado.getFecha()+"\t"+facturaEncabezado.getNombre()+"\t"+facturaEncabezado.getDireccion()+
+                                "\t"+facturaEncabezado.getNit());
+                        break;
+                        
+                        
+                    }
+
+            }
+
+        } while (opcion != 3);
+        System.out.println("Gracias");
+        System.out.println("Regrese pronto");
+
+    }
+
+}
