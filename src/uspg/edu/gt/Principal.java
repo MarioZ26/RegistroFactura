5885a23f8ce11c42269693ecd3ff454536c6b478
Josue <oeramirezz@alumno.uspg.edu.gt>
2020-10-17 15:47:02 -0600

    Nombre

--- src/uspg/edu/gt/Principal.java
+++ src/uspg/edu/gt/Principal.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Nombre       Carnet
+Otto Ramirez 2000136
  */
 package uspg.edu.gt;
 
